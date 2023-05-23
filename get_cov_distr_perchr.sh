5aa81c0e141e2ded93d6993c8a45e74ce8ef930d
James McK <43877269+jammc313@users.noreply.github.com>
2023-05-23 12:14:33 +0200

    Update get_cov_distr_perchr.sh

--- get_cov_distr_perchr.sh
+++ get_cov_distr_perchr.sh
@@ -3,18 +3,14 @@
 # Once complete, run "merge_cov_distr.py" to merge across chromosomes, and plot results with "plot_cov_distr.R".
 # The goal is to check reasonable site cov thresholds to use when filtering sites. Set these individually, rather than with a hard default such as 10<X<500. 
 
-#declare -a ind_1=( UstIshim Kotias sf12 Neanderthal flo011 )
-declare -a ind_1=( LBK Loschbour Bichon SIII )
+declare -a ind_1=( LBK Loschbour Bichon SIII UstIshim Kotias sf12 Neanderthal flo011 )
 
 for chr in {1..22}; do
     for (( i = 0; i < ${#ind_1[@]}; ++i )); do
 #        echo "${chr} ${ind_1[i]} ${ind_2[j]}"
 	(echo '#!/bin/bash -l'
-echo "module load conda"
-echo "source conda_init.sh"
-echo "conda activate poptrees"
 echo "python get_coverage_distr.py ${chr} ${ind_1[i]} 
-exit 0") | sbatch -p core -n 1 -t 1-00:00:00 -A p2018003 -J ${chr}_${ind_1[i]} -e DIR_error/${chr}_${ind_1[i]}.error
+exit 0") | sbatch -p core -n 1 -t 1-00:00:00 -A p2018 -J ${chr}_${ind_1[i]} -e DIR_error/${chr}_${ind_1[i]}.error
     done
 done
 
