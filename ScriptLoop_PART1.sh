1fed64d40cbe7227f3a762d62f8157860a410822
James McK <43877269+jammc313@users.noreply.github.com>
2023-05-23 12:08:41 +0200

    Update ScriptLoop_PART1.sh

--- ScriptLoop_PART1.sh
+++ ScriptLoop_PART1.sh
@@ -1,15 +1,12 @@
-#This script iterates 'Continuity_Anchor_Test_PART1.py' across all 22 chr and the given anchor individuals. 
+# This script iterates 'Continuity_Anchor_Test_PART1.py' across all 22 chr and the given anchor individuals. It searches through their vcfs, identifying reliable heterozygote ancestral/derived sites.
+# The jobs are run for each individual and each chromosome in parallel. 
 
-#declare -a anchorind=( flo011 Neanderthal Kotias sf12 UstIshim )
-declare -a anchorind=( LBK Loschbour Bichon SIII )
+declare -a anchorind=( LBK Loschbour Bichon SIII sf12 UstIshim Neanderthal )
 
 for chr in {1..22}; do
     for (( i = 0; i < ${#anchorind[@]}; ++i )); do
         (echo '#!/bin/bash -l'
-echo "module load conda"
-echo "source conda_init.sh"
-echo "conda activate poptrees"
 echo "python Continuity_Anchor_Test_PART1.py ${chr} ${anchorind[i]}
-exit 0") | sbatch -p core -n 1 -t 24:00:00 -A p2018003 -J ${chr}_${anchorind[i]} -o ${chr}_${anchorind[i]}.output -e DIR_error/${chr}_${anchorind[i]}.error
+exit 0") | sbatch -p core -n 1 -t 24:00:00 -A p2018 -J ${chr}_${anchorind[i]} -o ${chr}_${anchorind[i]}.output -e DIR_error/${chr}_${anchorind[i]}.error
     done
 done
